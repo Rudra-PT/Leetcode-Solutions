32cc44913985af5bbdb249068a3191fb6a30d0a6
Rudra Prakhar Tomar <rajputrudra940@gmail.com>
2026-01-11 10:05:46 +0530

    Time: 0 ms (100%), Space: 8.5 MB (31.87%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans = "";
+        int n = word1.length();
+        int m = word2.length();
+        for(int  i = 0  ; i <  n|| i<m ; i++ ){
+            if(i< n){
+                ans.push_back(word1[i]);
+            }
+            if(i<m){
+                ans.push_back(word2[i]);
+            }
+        }
+        return ans;
+    }
+};
