791c1a1d2de67a63068f1a3caeb81a477fd34470
Rudra Prakhar Tomar <rajputrudra940@gmail.com>
2026-05-24 21:57:31 +0530

    Time: 0 ms (100%), Space: 8.2 MB (59.75%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        priority_queue<pair<int, char>> maxHeap;
+        if (a > 0) maxHeap.push({a, 'a'});
+        if (b > 0) maxHeap.push({b, 'b'});
+        if (c > 0) maxHeap.push({c, 'c'});
+        
+        string result = "";
+        
+        while (!maxHeap.empty()) {
+            auto [freq1, char1] = maxHeap.top();
+            maxHeap.pop();
+            int n = result.length();
+            if (n >= 2 && result[n-1] == char1 && result[n-2] == char1) {
+                
+                if (maxHeap.empty()) {
+                    break;
+                }
+                
+                auto [freq2, char2] = maxHeap.top();
+                maxHeap.pop();
+                
+                result += char2;
+                freq2--;
+                
+                if (freq2 > 0) {
+                    maxHeap.push({freq2, char2});
+                }
+                
+                maxHeap.push({freq1, char1});
+                
+            } else {
+                result += char1;
+                freq1--;
+                
+                if (freq1 > 0) {
+                    maxHeap.push({freq1, char1});
+                }
+            }
+        }
+        
+        return result;
+    }
+};
