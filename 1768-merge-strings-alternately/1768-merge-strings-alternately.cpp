d82ce3345620c2308707b901e829b697ca1992fb
Rudra Prakhar Tomar <rajputrudra940@gmail.com>
2026-06-15 11:51:05 +0530

    Time: 0 ms (100%), Space: 8.6 MB (30.96%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string ans = "";
         int n = word1.length();
         int m = word2.length();
-        for(int  i = 0  ; i <  n|| i<m ; i++ ){
-            if(i< n){
-                ans.push_back(word1[i]);
+        string ans = "";
+
+        for (int i = 0; i < max(n, m); i++) {
+            if (i < n) {
+                ans += word1[i];
             }
-            if(i<m){
-                ans.push_back(word2[i]);
+            if (i < m) {
+                ans += word2[i];
             }
         }
         return ans;
