b59fd39878ca74049014c2d6cc389d9f98011837
Rudra Prakhar Tomar <rajputrudra940@gmail.com>
2026-06-25 07:29:22 +0530

    Time: 0 ms (100%), Space: 14.5 MB (41.08%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        int timeline[1001] = {0};
+        
+        for (const auto& trip : trips) {
+            int numPassengers = trip[0];
+            int from = trip[1];
+            int to = trip[2];
+            
+            timeline[from] += numPassengers; 
+            timeline[to] -= numPassengers; 
+        }
+        
+        int currentPassengers = 0;
+        for (int i = 0; i <= 1000; i++) {
+            currentPassengers += timeline[i];
+        
+            if (currentPassengers > capacity) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
