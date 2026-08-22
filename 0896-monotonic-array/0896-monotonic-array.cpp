f1ca1db616cb71bc56e5ddc00668270393ba7c8b
Rudra Prakhar Tomar <rajputrudra940@gmail.com>
2026-08-22 11:14:57 +0530

    Time: 0 ms (100%), Space: 100.2 MB (99.63%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int n = nums.size();
+        bool isIncreasing = true;
+        bool isDecreasing = true;
+        for(int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) {
+                isIncreasing = false;
+            }
+            if (nums[i] > nums[i - 1]) {
+                isDecreasing = false;
+            }
+        }
+        return isIncreasing || isDecreasing;
+    }
+};
